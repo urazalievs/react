2c53f1aebcd5d8cbd82ee652abb35f0f401d9d67
Sultanbek <sultanurazaliev1@gmail.com>
2025-01-09 14:02:25 +0300

    StepThree

--- src/pages/StepOne.jsx
+++ src/pages/StepOne.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ProgressBar } from "../components/ProgressBar"
 import { QuestionDiv } from "../components/QuestionDiv";
 import { AppLabel } from "../components/AppLable";
 import { AppButton } from "../components/AppButton";
+import { useNavigate } from "react-router-dom";
 
 const StepOne = () => {
+  //Ловит значение инпута
+  const [name, setName]= useState('')
+  //Показывает активна ли кнопка
+  const [btnErr, setBtnErr]= useState(true)
+  //Показывает наличие ошибки
+  const [nameErr, setNameErr]= useState(false)
+  const navigation = useNavigate()
+  const handClick = ()=>{
+    if(!name){
+      setBtnErr(true)
+      setNameErr(true)
+      navigation("/")
+    }else{
+      setBtnErr(false)
+      setNameErr(false)
+      navigation("/step-two")
+    }
+  }
+  useEffect(()=>{
+    if(!name){
+      setBtnErr(true)
+    }else{
+      setBtnErr(false)
+    }
+  },[name])
+  
   return (
     <div className="container">
       <div className="wrapper">
@@ -17,16 +44,20 @@ const StepOne = () => {
           <div className="question">
             <h2>1. Занимательный вопрос</h2>
             <AppLabel
-              labText
+              labText=''
               labName="answer"
               labErr={"Введите номер в правильном формате например"}
               labPlace={"Ваш ответ"}
               labType={"text"}
+              inpValue={name}
+              inpChange={setName}
+              hasError={nameErr}
             />
             <AppButton
               buttonText="Далее"
               buttonType="button"
-              isDisabled={true}
+              isDisabled={btnErr}
+              btnClick={handClick}
             />
           </div>
         </div>
